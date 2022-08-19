95b84690b97f7e7ce6c9790c09fb070153fbffe4
Noah VERTUEUX <81981323+vertueux@users.noreply.github.com>
2022-08-19 10:23:49 +0200

    Cleaned up the projet.
    To make a better file structure.

---
+++ src/Lights.js
@@ -0,0 +1,27 @@
+import React from "react"
+
+export function Lights() {
+  return (
+    <>
+      <spotLight
+        color={[1, 0.25, 0.7]}
+        intensity={1.5}
+        angle={0.6}
+        penumbra={0.5}
+        position={[5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+
+      <spotLight
+        color={[0.14, 0.5, 1]}
+        intensity={2}
+        angle={0.6}
+        penumbra={0.5}
+        position={[-5, 5, 0]}
+        castShadow
+        shadow-bias={-0.0001}
+      />
+    </>
+  );
+}
