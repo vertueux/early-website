d7e971fc44b47c35b1f222d4deaf9325d33d8adc
Vertueux <81981323+vertueux@users.noreply.github.com>
2023-11-21 18:55:09 +0100

    Updated the website navigation bar.
    Trying to make it more simple & aesthetic.

--- source/script.js
+++ source/script.js
@@ -1,19 +1,26 @@
 const hamburgerToggler = document.querySelector(".hamburger")
 const navLinksContainer = document.querySelector(".navlinks-container")
+let root = document.documentElement;
 
 const toggleNav = () => {
     hamburgerToggler.classList.toggle("open")
     const ariaToggle = hamburgerToggler.getAttribute("aria-expanded") === "true" ? "false" : "true";
     hamburgerToggler.setAttribute("aria-expanded", ariaToggle)
+    if (ariaToggle == "true") {
+        root.style.setProperty('--blurred-background', "blur(4px)");
+    } else {
+        root.style.setProperty('--blurred-background', "blur(0px)");
+    }
     navLinksContainer.classList.toggle("open")
 }
 
 hamburgerToggler.addEventListener("click", toggleNav)
 
 new ResizeObserver(entries => {
-    if (entries[0].contentRect.width <= 900) {
+    if (entries[0].contentRect.width <= 1200) {
         navLinksContainer.style.transition = "transform 0.2s ease-out"
     } else {
-        navLinksContainer.style.transition = "none"
+        navLinksContainer.style.transition = "none";
+        root.style.setProperty('--blurred-background', "blur(0px)");
     }
 }).observe(document.body)
