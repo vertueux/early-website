618bec6d8c538ba3f9b644da59b80b6d7df3de0b
Vertueux <81981323+vertueux@users.noreply.github.com>
2024-02-03 10:18:05 +0100

    Fixed the navbar on tiny devices.
    Updated script.js & navbar.css.

--- src/script.js
+++ src/script.js
@@ -4,7 +4,6 @@ const search = document.querySelector('.input-group input');
 const table_rows = document.querySelectorAll('tbody tr');
 const navbar = document.querySelector("nav");
 const page = document.querySelector(".page");
-const logo = document.getElementById("logo");
 const onHomePage = document.querySelector(".on-home-page");
 
 var navOpen = false;
@@ -20,6 +19,7 @@ const toggleNav = () => {
     if (navOpen == true) {
         // Closing the navbar.
         navOpen = false;
+        navLinksContainer.classList.add("close-nav");
     } else if (preventReopening == false) {
         navOpen = true;
     }
@@ -30,11 +30,9 @@ const toggleNav = () => {
         // Prevent from clicking.
         page.style.setProperty('pointer-events', "none");
         page.style.setProperty('filter', "brightness(0.3)");
-        logo.style.setProperty('filter', "brightness(0.3)");
     } else {
         page.style.setProperty('pointer-events', "auto");
         page.style.setProperty('filter', "brightness(1.0)");
-        logo.style.setProperty('filter', "brightness(1.0)");
     }
     navLinksContainer.classList.toggle("open");
 }
@@ -50,7 +48,7 @@ function closeNav() {
 
 document.addEventListener("click", (evt) => {
     var w = window.innerWidth;
-    if (w <= 1300) {
+    if (w <= 1450) {
         let targetEl = evt.target;    
         do {
             if(targetEl == navLinksContainer || targetEl == hamburgerToggler) {
@@ -65,23 +63,23 @@ document.addEventListener("click", (evt) => {
     }
 });
 new ResizeObserver(entries => {
-    if (entries[0].contentRect.width <= 1300) {
+    if (entries[0].contentRect.width <= 1450) {
         navLinksContainer.style.transition = "transform 0.1s ease-out";
         if (navOpen == true) {
             navLinksContainer.style.setProperty('display', "block");
             page.style.setProperty('pointer-events', "none");
+            navLinksContainer.classList.add("close-nav");
             document.body.classList.add("stop-scrolling");
             page.style.setProperty('filter', "brightness(0.3)");
-            logo.style.setProperty('filter', "brightness(0.3)");
         }
     } else {
         navLinksContainer.style.transition = "none";
         if (navOpen == true) {
             navLinksContainer.style.setProperty('display', "none");
+            navLinksContainer.classList.remove("close-nav");
             document.body.classList.remove("stop-scrolling");
             page.style.setProperty('pointer-events', "auto");
             page.style.setProperty('filter', "brightness(1.0)");
-            logo.style.setProperty('filter', "brightness(1.0)");
         }
     }
 }).observe(document.body)
